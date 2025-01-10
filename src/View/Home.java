7eb3c050f9e41f328cf53613adfb745019927bea
Guille <programoreno@gmail.com>
2025-01-10 18:03:38 +0100

    Scaner creado y mejorado el codigo

--- src/View/Home.java
+++ src/View/Home.java
@@ -1,18 +1,35 @@
 package View;
 
+import java.util.Scanner;
+
 import Model.Empleado;
 
 public class Home {
 
 	public static void main(String[] args) {
 
+		//cracion del Scaner
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("Cual es tu nombre: ");
+		String name = scan.nextLine();
+		
+		System.out.println("Hola, " + name + "!\n");
+		
+		//Cerramos Scaner
+		scan.close();
+		
+		
+		//Creaicon empleado
 		Empleado emp1 = new Empleado() {};
-		Empleado emp2 = new Empleado("0x","Guille","Moreno",12.0);
+		Empleado emp2 = new Empleado("000000000X","Guillermo","Moreno",12.0);
+		
 		
 		VerEmple(emp2);
 		
 	}
 	
+	//Ver datos empleado
 	public static void VerEmple(Empleado emp) {
 		System.out.println("*** DATOS EMPLEADO ***");
 		System.out.println("DNI -> "+emp.getDni());
